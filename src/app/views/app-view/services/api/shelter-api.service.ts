eed1590bb309c3bbe1e9b46e6bbf942048a3e807
Łukasz Fereniec <lukaszfereniec@interia.pl>
2023-01-13 20:52:08 +0100

    added service for shelter api

---
+++ src/app/views/app-view/services/api/shelter-api.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { ShelterResponse } from 'backend/src/models/ShelterModel';
+import { Observable } from 'rxjs';
+import { ApiService } from 'src/app/services/api.service';
+
+@Injectable()
+export class ShelterApiService extends ApiService {
+  constructor(private readonly http: HttpClient) {
+    super();
+  }
+
+  public getShelters(): Observable<ShelterResponse[]> {
+    return this.http.get<ShelterResponse[]>(`${this.rootUrl}/shelters`);
+  }
+}
